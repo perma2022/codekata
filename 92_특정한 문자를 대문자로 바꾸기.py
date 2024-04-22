2af6d12472da2a02a7b967eb9e64b437544f2c8d
shinjihye <121236442+perma2022@users.noreply.github.com>
2024-04-22 10:38:20 +0900

    Add files via upload

---
+++ 92_특정한 문자를 대문자로 바꾸기.py
@@ -0,0 +1,9 @@
+def solution(my_string, alp):
+    answer = ''
+
+    for char in my_string:
+        if char == alp:
+            answer += char.upper()
+        else:
+            answer += char
+    return answer
