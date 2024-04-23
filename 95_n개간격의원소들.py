c4eaac169168c3a2572d7749609d146d0fda0826
shinjihye <121236442+perma2022@users.noreply.github.com>
2024-04-23 10:28:58 +0900

    Add files via upload

---
+++ 95_n개간격의원소들.py
@@ -0,0 +1,11 @@
+def solution(num_list, n):
+    answer = []
+
+    for i in range(0, len(num_list), n):
+        answer.append(num_list[i])
+
+    return answer
+
+
+def solution(num_list, n):
+    return num_list[::n]
